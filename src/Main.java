934655bc5a477b819f9bb101fb87ef282f4a01c1
SONEKT <shulmanlevgen@gmail.com>
2023-05-26 14:35:07 +0500

    i

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        byte b = 3;
+        short s = 2;
+        int i = 5;
+        long l = 9;
+        float f = 12;
+        double d = 13;
+        char c = 14;
+        boolean bo = true;
+        System.out.println("Значение переменной byte с типом целочислительные равно " + b);
+    }
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+    public static void task1(){}
+
+}
